aaf6261008c327f2497e1d78aa04f715bf369158
metamn <bartus.csongor@gmail.com>
2013-08-13 17:45:20 +0300

    Scrolling navigation added

--- assets/jquery.init.js
+++ assets/jquery.init.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
 
+
+  // Scroll to features
+  $('#intro form input').click(function() {
+    $("html, body").animate({ scrollTop: $('#features').offset().top }, "slow");
+    return false;
+  });
+
   // Show testimonials
   $('#features nav').click(function() {
     $('#testimonials').slideToggle();
-    scrollBottom();
+    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
   });
   
-    
-  // Scroll to bottom
-  function scrollBottom() {
-    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
-  }
+  
 
 });
