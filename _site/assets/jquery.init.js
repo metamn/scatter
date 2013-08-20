c8b2962a4f4373154fb06ff19332458e7cb13d95
metamn <bartus.csongor@gmail.com>
2013-08-20 13:51:50 +0300

    Working hard on the animations ....

--- _site/assets/jquery.init.js
+++ _site/assets/jquery.init.js
@@ -3,15 +3,6 @@ $(document).ready(function() {
   // Animations on the business page
   
   
-  // Second slide
-  
-  setInterval(function(){
-    $('.business #slide-2 div').first().addClass('highlight', 2000, function() {
-      $(this).removeClass('highlight', 2000);
-    });
-  }, 4000);
- 
-  
   // First slide
   $('.business #slide-1 div').hide();
   $('.business #slide-1 div').each(function(index) {
@@ -24,69 +15,16 @@ $(document).ready(function() {
         break;
       case 1:
         var top = "-=100";
-        var left = "+=50";
+        var left = "+=70";
         break;
        case 2:
         var top = "+=30";
-        var left = "+=100";
+        var left = "+=50";
         break;
     }
     
     fly(div, top, left);
   }); 
-  
-  
-  // Change device border color
-  function borderColor(div, speed, color) {
-    div.animate({
-      borderRightColor: color
-    }, speed, function() {
-      // Animation complete
-    });
-    
-    div.animate({
-      borderBottomColor: color
-    }, speed, function() {
-      // Animation complete
-    });
-    
-    div.animate({
-      borderLeftColor: color
-    }, speed, function() {
-      // Animation complete
-    });
-    
-    div.animate({
-      borderTopColor: color
-    }, speed, function() {
-      arrow(div, speed, color);
-    });
-  }
-  borderColor($('.business #slide-1 div').first(), 700, '#77C8F5');
-  
-  
-  // Change arrow color
-  function arrow(div, speed, color) {
-    if (color == '#77C8F5') {
-      div.addClass('highlight');
-    } else {
-      div.removeClass('highlight');
-    }
-  
-    if (div.attr('id') == $('.business #slide-1 div').last().attr('id')) {
-      d = $('.business #slide-1 div').first();
-      if (color == '#77C8F5') {
-        color = '#F2C7C4';
-      } else {
-        color = '#77C8F5';
-      }
-    } else {
-      d = div.next();
-    }
-    
-    borderColor(d, speed, color);
-  }
-  
   
   
   // Fly in devices
@@ -98,11 +36,30 @@ $(document).ready(function() {
       left: left,
       top: top
     }, 1000, function() {
-      // Animation complete
+      div.children('span').animate({
+        opacity: 1
+      }, 3000, function() {
+        // Animation complete
+      });
     });
   }
  
+  // Rotate device descriptions
+  function rotate() {
+    var div = $('.business #slide-1 div').first();
+    var span = div.children('span');
+    
+    var position = div.next().children('span').position();
+    
+    span.animate({
+      top: position.top,
+      left: position.left
+    }, 1000, function() {
+      // 
+    });
+  }
   
+  //rotate();
   
   
   // Scrolling on the business page
