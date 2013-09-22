f9a2a9da5bb72121fd8b20486c7a6b2ea9c05903
metamn <bartus.csongor@gmail.com>
2013-09-22 12:04:28 +0300

    Started to add events on the card

--- _site/assets/jquery.main.js
+++ _site/assets/jquery.main.js
@@ -1,3 +1,21 @@
+$(document).ready(function() {
+
+  // Click on follow
+  $('.main article .cardfooter .l_followbtn').click(function() {
+    $(this).toggleClass('active');
+    return false;
+  });
+  
+  
+  // Click on company name
+  $('.main article .usercontainer').click(function() {
+    $(this).parent().parent().toggleClass('active');
+    $(this).children('.companyinfo').slideToggle('slow');   
+  });
+
+});
+
+
 var container = document.querySelector('.main #content');
 var msnry = new Masonry( container, {
   // options
