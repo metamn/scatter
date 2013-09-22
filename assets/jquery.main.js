d5277b78013a7650537e351c3904a58ba4871da4
metamn <bartus.csongor@gmail.com>
2013-09-22 12:48:01 +0300

    All events added to cart

--- assets/jquery.main.js
+++ assets/jquery.main.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
 
+  // Close  map
+  $('.main #map span').click(function() {
+    $('.main #map').slideToggle('slow');
+    return false;
+  });
+
+  // Click on map
+  $('.main article .cardfooter .mapicon').click(function() {
+    $('.main #map').slideToggle('slow');
+    return false;
+  });
+
+  // Click on photo
+  $('.main article .cardfooter .l_camera').click(function() {
+    $(this).parent().parent().parent().toggleClass('active');
+    $(this).parent().parent().children('.companyphoto').slideToggle('slow');
+    return false;
+  });
+
+  // Click on message
+  $('.main article .cardfooter .l_chat').click(function() {
+    $(this).parent().parent().parent().toggleClass('active');
+    $(this).parent().parent().children('.messagecontainer').children('.sendmessage').slideToggle('slow');
+    return false;
+  });
+  
+
   // Click on follow
   $('.main article .cardfooter .l_followbtn').click(function() {
     $(this).toggleClass('active');
