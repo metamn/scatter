d1f5dc1dd14db6056fe4b49f87d89b07d2c4f564
metamn <bartus.csongor@gmail.com>
2013-09-24 10:34:46 +0300

    Final touches on card design

--- _site/assets/jquery.main.js
+++ _site/assets/jquery.main.js
@@ -6,30 +6,55 @@ $(document).ready(function() {
     $(this).toggleClass('active');
     $('.main article').toggleClass('hidden');
   });
+  
+  
+  // Click on camera
+  $('.main article .messagecontainer .messagephoto-icon').click(function() {
+    cardActive($(this).parent().parent().parent(), $(this).next());
+  });
+  
+  // Click on company details
+  $('.main article .usercontainer').click(function() {
+    cardActive($(this).parent().parent(), $(this).children('.companyinfo'));
+  });
 
   // Click on map
-  $('.main article .cardfooter .mapicon, .main #map span').click(function() {
-    $('.main article').toggleClass('hidden');
-    
-    $(this).parent().parent().parent().toggleClass('expanded');
-    $(this).parent().parent().children('#map').slideToggle();
+  $('.main article .cardfooter .mapicon, .main #map').click(function() {
+    $('.main #map').slideToggle('slow');
     return false;
   });
 
   // Click on message
   $('.main article .cardfooter .l_chat').click(function() {
-    $(this).parent().parent().parent().toggleClass('active');
-    $(this).parent().parent().children('.messagecontainer').children('.sendmessage').slideToggle('slow');
+    cardActive($(this).parent().parent().parent(), $(this).parent().parent().children('.messagecontainer').children('.sendmessage'));
     return false;
   });
   
-
   // Click on follow
   $('.main article .cardfooter .l_followbtn').click(function() {
     $(this).toggleClass('active');
     return false;
   });
   
+  
+  // Make the card active or inactive
+  function cardActive(card, item) {
+    var on = item.is(':visible');
+    var counter = 0;
+    card.children().children('.usercontainer').children('.companyinfo').is(':visible') ? counter += 1 : counter = counter;
+    card.children().children('.messagecontainer').children('.sendmessage').is(':visible') ? counter += 1 : counter = counter;
+    card.children().children('.messagecontainer').children('.messagephoto').is(':visible') ? counter += 1 : counter = counter;
+    
+    item.slideToggle('slow');
+    
+    if (on) {
+      if (counter == 1) {
+        card.removeClass('active');
+      }
+    } else {
+      card.addClass('active');
+    }
+  }
 
 });
 
